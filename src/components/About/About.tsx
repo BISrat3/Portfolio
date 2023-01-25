fa896550a6c6f0e31392b412d4417751503e77a1
bisrat <bisrat.amtataw@gmail.com>
2023-01-25 16:41:25 -0500

    update pages

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -7,7 +7,7 @@ function About() {
         <div className={styles.aboutContainer} id='about'>
             <img src={profile} alt='Bisrat profile' className={styles.portrait} />
             <h1 className={styles.aboutHeader}>ABOUT ME</h1>
-            <p className={styles.aboutDesc}>Hello! I'm Bisrat, a software engineer. I have four years of full-stack development experience. In my previous role, I worked on several projects, like leave management system, share management, network monitoring system, etc. My communication skills are strong. That makes me an excellent communicator in the workplace. I also have a bachelor's degree in information systems from the university of Mekelle, Ethiopia and a master's degree in Software Engineering. I have completed two certifications; I'm  a Certified Ethical Hacker from EC-Council and Certified Software Engineering Immersive from General Assembly Bootcamp.</p>
+            <p className={styles.aboutDesc}>Hello! I'm Bisrat, a software engineer. I have four years of experience in a full-stack position. In my previous role, I worked on several projects, like leave management system, share management, network monitoring system, etc. I have excellent problem-solving and communication skills. That makes me an excellent candidate for the company I work for. I have a bachelor's degree in information systems from the university of Mekelle, Ethiopia and a master's degree in Software Engineering. Currently, I have completed two certifications; I'm  a Certified Ethical Hacker from EC-Council and Certified Software Engineering Immersive from General Assembly Bootcamp.</p>
             <br />
         </div>
     );
