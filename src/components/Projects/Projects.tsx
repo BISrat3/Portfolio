c69d7a362cd18fdcb567de6e707dc9097ab2905d
bisrat <bisrat.amtataw@gmail.com>
2023-02-02 22:32:31 -0500

    project update

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -41,7 +41,7 @@ function Projects() {
                 <div className={styles.projectImgContainer}><img src={LinkedInclone} alt='LinkedIn clone application ' className={styles.projectImg} /></div>
                 <div className={styles.project}>
                     <h2 className={styles.projectHeader}>LinkedIn Clone</h2>
-                    <p className={`${styles.projectDesc} ${styles.projectFourDesc}`}>My app is called LinkedIn Clone App. </p>
+                    <p className={`${styles.projectDesc} ${styles.projectFourDesc}`}>My app is called LinkedIn Clone App. The app allows the user to post once they signed in. </p>
                     <div className={styles.projectLinkContainer3}>
                         <a href="https://linkedin-clone-51f76.web.app" target='_blank' rel='noreferrer' className={styles.projectLink}>Live</a>
                         <a href='https://github.com/BISrat3/linkedin-clone'
