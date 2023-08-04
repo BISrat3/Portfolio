5b7c9cf01b1c520c894372b473d9f862b837e0db
bisrat <bisrat.amtataw@gmail.com>
2023-08-04 14:02:55 -0400

    update

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -7,7 +7,7 @@ function About() {
         <div className={styles.aboutContainer} id='about'>
             <img src={profile} alt='Bisrat profile' className={styles.portrait} />
             <h1 className={styles.aboutHeader}>ABOUT ME</h1>
-            <p className={styles.aboutDesc}>Hello! I'm Bisrat, a software engineer. I have four years of experience in a full-stack position. In my previous role, I worked on several projects, like leave management system, share management, network monitoring system, etc. I have excellent problem-solving and communication skills. That makes me an excellent candidate for the company I work for. I have a bachelor's degree in information systems from the university of Mekelle, Ethiopia and a master's degree in Software Engineering. Currently, I have completed two certifications; I'm  a Certified Ethical Hacker from EC-Council and Certified Software Engineering Immersive from General Assembly Bootcamp.</p>
+            <p className={styles.aboutDesc}>Hello! I'm Bisrat, a software engineer. I have 6+ years of experience in a full-stack. r, I have successfully utilized my expertise in HTML5, CSS3, and JavaScript to create visually appealing and interactive user interfaces. I have extensive experience with popular frameworks such as React JS, React Hooks, and Redux, and have developed numerous front end web applications using these technologies. I have a deep understanding of React JS and its core principles, allowing me to optimize and benchmark applications for enhanced performance.</p>
             <br />
         </div>
     );
